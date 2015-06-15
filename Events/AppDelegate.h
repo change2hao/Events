d957aa07afa0ddf5e4a5c0589bfd2707fe4f366e
Administrator <lwtiandev@gmail.com>
2015-06-15 09:36:24 +0800

    Initial Commit

---
+++ Events/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Events
+//
+//  Created by tianliwei on 6/15/15.
+//  Copyright (c) 2015 Beijing Jaeger Communication Electronic Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Events/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Events
+//
+//  Created by tianliwei on 6/15/15.
+//  Copyright (c) 2015 Beijing Jaeger Communication Electronic Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
